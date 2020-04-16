5d6ceb4f738b8755f429f58b5981e60570ed69a3
Mike Fehrenbach <fehrenbach.mike@gmail.com>
2020-04-15 20:49:55 -0400

    Add in the viewset, route and model for listing ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,10 @@ from rest_framework import mixins, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import (
+  Tag,
+  Ingredient
+)
 from recipe import serializers
 
 
@@ -19,3 +22,14 @@ class TagViewSet(viewsets.GenericViewSet,
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-name')
