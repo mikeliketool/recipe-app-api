a6df7568061a17bda5ed3c2732270319a7cfcd12
Mike Fehrenbach <fehrenbach.mike@gmail.com>
2020-04-21 20:33:30 -0400

    Add image to recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
+from unittest.mock import patch
+
 from django.contrib.auth import get_user_model
 from django.core.validators import ValidationError
+from django.test import TestCase
 
 from core import models
 
@@ -71,3 +73,12 @@ class ModelTests(TestCase):
       )
 
       self.assertEqual(str(recipe), recipe.title)
+
+  @patch('uuid.uuid4')
+  def test_recipe_file_name_uuid(self, mock_uuid):
+      uuid = 'test-uuid'
+      mock_uuid.return_value = uuid
+      file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+
+      expected_path = f'uploads/recipe/{uuid}.jpg'
+      self.assertEqual(file_path, expected_path)
